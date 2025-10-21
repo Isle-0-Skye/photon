349bc9176d689aecbb8c598377bfb9754edd2e1a
Isle-0-Skye <Isle0Skye@proton.me>
2025-10-21 16:20:37 +0100

    added support for self updating

--- cli/main.py
+++ cli/main.py
@@ -5,19 +5,21 @@ from enum import Enum
 
 from photon import PHOTON_LOGGER
 from photon.lib.notify import notify
-from photon.lib.package import Package
+from photon.lib.package import Package, PhotonPackage
 
 
 # < ----------------------------------------------------------------------- > #
 
 
 class Mode(Enum):
-    HELP = 0
-    VERSION = 1
-    RUN = 2
-    UPDATE = 3
-    INSTALL = 4
+    # fmt:off
+    HELP      = 0
+    VERSION   = 1
+    RUN       = 2
+    UPDATE    = 3
+    INSTALL   = 4
     UNINSTALL = 5
+    # fmt:on
 
 
 # < ----------------------------------------------------------------------- > #
@@ -27,6 +29,32 @@ class Mode(Enum):
 class Settings:
     MODE: Mode
     PACKAGE: Package
+    LOG_LEVEL: int = 20
+
+
+# < ----------------------------------------------------------------------- > #
+
+
+def strToLogInt(level: str) -> int:
+    match level.upper():
+        case "DEBUG":
+            return 10
+
+        case "INFO":
+            return 20
+
+        case "WARNING":
+            return 30
+
+        case "ERROR":
+            return 40
+
+        case "CRITICAL":
+            return 50
+
+        case _:
+            PHOTON_LOGGER.debug("unknown log level, fallback to 0")
+            return 0
 
 
 # < ----------------------------------------------------------------------- > #
@@ -71,7 +99,7 @@ def main() -> int:
     ]
 
     supported_options: list[str] = [
-        "--package",
+        "--log-level",
     ]
 
     # < [0] / 1: path to __main__.py file > #
@@ -90,28 +118,62 @@ def main() -> int:
         mode: Mode = strToMode(sys.argv[1])
 
     # < package to run > #
-    package = Package(sys.argv[2])
+    package: Package | PhotonPackage
+    if sys.argv[2].endswith(".photon"):
+        PHOTON_LOGGER.info("using alternative package class")
+        package = PhotonPackage(sys.argv[2])
+    else:
+        package = Package(sys.argv[2])
+
+    # < settings for photon > #
     settings = Settings(mode, package)
 
     # < split args between photon and the package > #
     # < -- is used to split > #
-    photon_args: list[str] = []
+    photon_args: dict[str, str] = {}
     package_args: list[str] = []
 
+    index = 2
+    skip_next = False
+    stop = False
     for_photon = True
 
     for arg in sys.argv[3:]:
+        index = index + 1
+
+        if skip_next:
+            skip_next = False
+            continue
+
         if arg == "--":
             for_photon = False
             continue
 
         if for_photon and arg in supported_options:
-            photon_args.append(arg)
+            photon_args[arg] = sys.argv[index + 1]
+            skip_next = True
         elif for_photon:
             PHOTON_LOGGER.warning(f"unsupported option: {arg}")
-            return 1
+            stop = True
         else:
             package_args.append(arg)
+
+    if stop:
+        return 1
+
+    # < set photons log level > #
+    if "--log-level" in photon_args:
+        log_level = photon_args["--log-level"]
+        log_int = strToLogInt(log_level)
+
+        settings.LOG_LEVEL = log_int
+
+    PHOTON_LOGGER.setLevel(settings.LOG_LEVEL)
+
+    # < useful info for debugging > #
+    PHOTON_LOGGER.debug(f"running photon with args: {photon_args}")
+    PHOTON_LOGGER.debug(settings)
+    PHOTON_LOGGER.debug(f"running package with args: {package_args}")
 
     # < run program in desired mode > #
     match settings.MODE:
--- lib/notify.py
+++ lib/notify.py
@@ -1,5 +1,8 @@
 import subprocess
 import sys
+import time
+
+from photon import PHOTON_LOGGER
 
 
 if sys.platform == "win32":
@@ -23,13 +26,17 @@ def _notify_win32(message: str, title: str = "") -> None:
 # < ----------------------------------------------------------------------- > #
 
 
-def notify(message: str, title: str = "") -> None:
+def notify(message: str, title: str = "", sleep: int = 3) -> None:
+    time.sleep(sleep)
     match sys.platform:
         case "linux":
             _notify_linux(title, message)
 
         case "win32":
             _notify_win32(title, message)
+
+        case _:
+            PHOTON_LOGGER.warning("unsupported notify platform")
 
 
 # < ----------------------------------------------------------------------- > #
